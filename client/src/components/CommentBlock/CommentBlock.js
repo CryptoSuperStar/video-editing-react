2d33cccf7eb2bb95fd80042219257be34cc905c6
dev3provid <dev3provid@gmail.com>
2021-11-08 17:19:07 +0530

    fixed CommentBlock.js bug

--- client/src/components/CommentBlock/CommentBlock.js
+++ client/src/components/CommentBlock/CommentBlock.js
@@ -7,15 +7,15 @@ const CommentBlock = ({ arrComments }) => {
     <div className="CommentBlock">
       <h5>Pinned Notes</h5>
       {arrComments.map((scr, i) => (
-        scr.length && scr.map((item, i) => {
-          return (item.text.length > 0 &&
+        scr.length ? scr.map((item, i) => {
+          return (item.text.length > 0 ?
             <div className="comment__item" key={i}>
               <div className="comment_time">{item.time}</div>
               <div className="comment__text">{item.text}</div>
               <div className="comment__date">{moment(item.createdAt).format('Do MMMM, YYYY hh:mm a')}</div>
-            </div>
+            </div> : ""
           )
-        })
+        }) : ''
       ))}
     </div>
   );
