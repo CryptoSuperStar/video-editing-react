0b110fc08dad5d9433da14507235f59200257fc9
dev3provid <dev3provid@gmail.com>
2021-11-17 13:20:27 +0530

    Jira task MVP-92 and MVP-99

--- client/src/components/Modals/PayWallModal.js
+++ client/src/components/Modals/PayWallModal.js
@@ -1,39 +1,41 @@
-import React, {useEffect, useState} from 'react';
-import {connect} from "react-redux";
-import {ReactComponent as Cancel} from "../../assets/img/close-2.svg";
-import {ReactComponent as Tick} from "../../assets/img/accept_added_check_complite_yes_icon.svg";
-import {CardCvcElement, CardExpiryElement, CardNumberElement, PaymentRequestButtonElement, useElements,
-  useStripe} from "@stripe/react-stripe-js";
+import React, { useEffect, useState } from 'react';
+import { connect } from "react-redux";
+import { ReactComponent as Cancel } from "../../assets/img/close-2.svg";
+import { ReactComponent as Tick } from "../../assets/img/accept_added_check_complite_yes_icon.svg";
+import {
+  CardCvcElement, CardExpiryElement, CardNumberElement, PaymentRequestButtonElement, useElements,
+  useStripe
+} from "@stripe/react-stripe-js";
 import PaypalExpressBtn from "react-paypal-express-checkout";
-import {toast} from "react-toastify";
+import { toast } from "react-toastify";
 import axios from "axios";
 import moment from "moment";
-import {REACT_APP_PAYPAL_API,REACT_APP_API_URL} from "../../utils/misc";
-import {ReactComponent as CreditCard} from "../../assets/img/credit-card.svg";
-import {ReactComponent as PayPal} from "../../assets/img/paypal.svg";
-import {ReactComponent as ApplePay} from "../../assets/img/apple-pay.svg";
-import {ReactComponent as ArrowLeft} from "../../assets/img/arrow-left.svg";
-import {updateUser} from "../../store/actions/auth.action";
+import { REACT_APP_PAYPAL_API, REACT_APP_API_URL } from "../../utils/misc";
+import { ReactComponent as CreditCard } from "../../assets/img/credit-card.svg";
+import { ReactComponent as PayPal } from "../../assets/img/paypal.svg";
+import { ReactComponent as ApplePay } from "../../assets/img/apple-pay.svg";
+import { ReactComponent as ArrowLeft } from "../../assets/img/arrow-left.svg";
+import { updateUser } from "../../store/actions/auth.action";
 import './PayWallModal.scss';
 import penDot from "../../assets/img/penDot.png";
 
 const PayWallModal = (props) => {
-  
+
   const stripe = useStripe();
   const elements = useElements();
-  
+
   let payment = {
-      paymentId: '',
-      holder: '',
-      cardNumberLast4str: '',
-      paidBy: null,
-      plan: {
-        title: '',
-        price: '',
-        totalCost: '',
-        paidDate: '',
-        paidExpiresDate: ''
-      }
+    paymentId: '',
+    holder: '',
+    cardNumberLast4str: '',
+    paidBy: null,
+    plan: {
+      title: '',
+      price: '',
+      totalCost: '',
+      paidDate: '',
+      paidExpiresDate: ''
+    }
   }
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
@@ -42,13 +44,13 @@ const PayWallModal = (props) => {
     cost: 49,
     active: true,
     totalPrice: 588
-  },{
+  }, {
     title: 'MONTHLY',
     cost: 75,
     active: false,
     totalPrice: 75
   }]);
-  
+
   const [payments, setPayments] = useState([{
     title: "credit card",
     src: <CreditCard />,
@@ -64,7 +66,7 @@ const PayWallModal = (props) => {
   }]);
   const [activePayment, setActivePayment] = useState('credit card');
   const [paymentRequest, setPaymentRequest] = useState(null);
-  
+
   let env = 'sandbox';
   let currency = 'USD';
   let total = plans.filter(plan => plan.active === true)[0].totalPrice;
@@ -72,7 +74,7 @@ const PayWallModal = (props) => {
     sandbox: REACT_APP_PAYPAL_API,
     production: ''
   };
-  
+
   useEffect(() => {
     if (stripe) {
       const pr = stripe.paymentRequest({
@@ -88,33 +90,33 @@ const PayWallModal = (props) => {
       // Check the availability of the Payment Request API first.
       pr.canMakePayment().then((result) => {
         if (result) {
-          console.log('result2',result)
+          console.log('result2', result)
           pr.on('paymentmethod', handlePaymentMethodReceived);
           setPaymentRequest(pr);
         }
       });
     }
   }, [stripe])
-  
+
   const handlePaymentMethodReceived = async (event) => {
     // Send the payment details to our function.
     const paymentDetails = {
       payment_method: event.paymentMethod.id
     };
     const response = await axios.post(`${REACT_APP_API_URL}/create-payment-intent`,
-      {paymentDetails, total}).then((response) => {
+      { paymentDetails, total }).then((response) => {
         //SUCCESS: put client secret and id into an object and return it
         return {
           secret: response.data.client_secret,
           id: response.data.intent_id,
         };
       },
-      (error) => {
-        //ERROR: log the error and return
-        toast.error(error)
-        return error;
-      }
-    );
+        (error) => {
+          //ERROR: log the error and return
+          toast.error(error)
+          return error;
+        }
+      );
     // Report to the browser that the confirmation was successful, prompting
     // it to close the browser payment method collection interface.
     event.complete('success');
@@ -122,7 +124,7 @@ const PayWallModal = (props) => {
     const result = await stripe.confirmCardPayment(
       response.secret
     );
-    
+
     if (result.error) {
       console.log(result.error);
       toast.error(result.error)
@@ -136,14 +138,14 @@ const PayWallModal = (props) => {
       );
     }
   };
-  
+
   const changePlan = i => {
     let newPlans = [...plans];
     newPlans.map(plan => plan.active = false);
     newPlans[i].active = true;
     setPlans(newPlans);
   }
-  
+
   const changePayment = i => {
     let newPayments = [...payments];
     newPayments.map(pay => pay.active = false);
@@ -151,10 +153,10 @@ const PayWallModal = (props) => {
     setActivePayment(newPayments[i].title);
     setPayments(newPayments);
   }
-  
+
   const collectPaymentsData = (paymentId) => {
     let plan = plans.filter(plan => plan.active)[0];
-    
+
     const paymentData = [{
       ...payment,
       paidBy: payment.paidBy ? payment.paidBy : activePayment,
@@ -177,27 +179,28 @@ const PayWallModal = (props) => {
     console.log(newUserInfo);
     props.dispatch(updateUser(props.user._id, newUserInfo, props.setShowPayWall))
   }
-  
+
   const onSuccess = (pay) => {
     // Congratulation, it came here means everything's fine!
     console.log("The payment was succeeded!", pay);
     if (payment.paid) {
       const [lastName, firstName] = pay.address.recipient_name.split(' ');
-      payment = {...payment,
+      payment = {
+        ...payment,
         holder: firstName + ' ' + lastName
       }
       collectPaymentsData(pay.paymentID, firstName, lastName)
     }
     // You can bind the "payment" object's value to your state or props or whatever here, please see below for sample returned data
   }
-  
+
   const onCancel = (data) => {
     // User pressed "cancel" or close Paypal's popup!
     console.log('The payment was cancelled!', data);
     toast.warning('The payment was cancelled!')
     // You can bind the "data" object's value to your state or props or whatever here, please see below for sample returned data
   }
-  
+
   const onError = (err) => {
     // The main Paypal's script cannot be loaded or somethings block the loading of that script!
     console.log("Error!", err);
@@ -205,7 +208,7 @@ const PayWallModal = (props) => {
     // Because the Paypal's main script is loaded asynchronously from "https://www.paypalobjects.com/api/checkout.js"
     // => sometimes it may take about 0.5 second for everything to get set, or for the button to appear
   }
-  
+
   const handleSubmit = async (event) => {
     event.preventDefault();
     if (!stripe || !elements) {
@@ -214,37 +217,37 @@ const PayWallModal = (props) => {
     if (activePayment === 'credit card') {
       //STEP 1:
       //create new payment method based on card and form information
-      const {error, paymentMethod} = await stripe.createPaymentMethod({
+      const { error, paymentMethod } = await stripe.createPaymentMethod({
         type: 'card',
         card: elements.getElement(CardNumberElement)
       });
       setActivePayment(paymentMethod.card.brand)
-      payment = {...payment, cardNumberLast4str: paymentMethod.card.last4, paidBy: paymentMethod.card.brand};
+      payment = { ...payment, cardNumberLast4str: paymentMethod.card.last4, paidBy: paymentMethod.card.brand };
       if (error) {
         console.log('[error]', error);
         toast.error(error)
       }
       //STEP 2:
       //create a new payment request and get irs client secret + id from the server
-      const intentData = await axios.post(`${REACT_APP_API_URL}/stripe`, {total})
+      const intentData = await axios.post(`${REACT_APP_API_URL}/stripe`, { total })
         .then((response) => {
-          
-            //SUCCESS: put client secret and id into an object and return it
-            return {
-              secret: response.data.client_secret,
-              id: response.data.intent_id,
-            };
-          },
+
+          //SUCCESS: put client secret and id into an object and return it
+          return {
+            secret: response.data.client_secret,
+            id: response.data.intent_id,
+          };
+        },
           (error) => {
             //ERROR: log the error and return
             toast.error(error)
             return error;
           }
         );
-      
+
       //STEP 3:
       //confirm the payment and use the new payment method
-      const result = await stripe.confirmCardPayment(intentData.secret, {payment_method: paymentMethod.id});
+      const result = await stripe.confirmCardPayment(intentData.secret, { payment_method: paymentMethod.id });
       //handle errors again
       if (result.error) {
         toast.error(result.error)
@@ -254,11 +257,11 @@ const PayWallModal = (props) => {
       // The payment has been processed! send a confirmation to the server and register user
       if (result.paymentIntent.status === "succeeded") {
         const confirmedPayment = await axios.post(`${REACT_APP_API_URL}/confirm-payment`, {
-            //include id of payment
-            payment_id: intentData.id,
-            payment_type: "stripe",
-            //send any other data here
-          })
+          //include id of payment
+          payment_id: intentData.id,
+          payment_type: "stripe",
+          //send any other data here
+        })
           .then(
             (response) => {
               //SUCCESS: return the response message
@@ -277,13 +280,13 @@ const PayWallModal = (props) => {
       }
     }
   };
-  
+
   return (
     <div className="modal__wrapper">
       <div className="pay__wall--modal">
-        <div className="connectSocial__cross" onClick={() => props.setShowPayWall(false)}>
-          <Cancel fill="black" className="connectSocial__cross--cancel"/>
-          <ArrowLeft className="connectSocial__cross--arrowLeft"/>
+        <div className="connectSocial__cross" onClick={() => { props.setShowPayWall(false); props.setShowPayAccess(true) }}>
+          <Cancel fill="black" className="connectSocial__cross--cancel" />
+          <ArrowLeft className="connectSocial__cross--arrowLeft" />
         </div>
         <h3>Buy Pro</h3>
         <p>Lorem ipsum dolor sit amet</p>
@@ -294,13 +297,13 @@ const PayWallModal = (props) => {
               border: `1px solid ${plan.active ? "#3b8590" : '#36596a55'}`,
               backgroundColor: plan.active ? '#3b85911a' : 'white'
             }}
-                 onClick={() => changePlan(i)}
+              onClick={() => changePlan(i)}
             >
               <div className="modal__plans--title">
                 <strong>{plan.title}</strong>
                 <div className="modal__plans--tick"
-                     style={{backgroundColor: plan.active ? "#3b8590" : "rgba(133,134,149,0.1)"}}>
-                  {plan.active && <Tick fill="white"/>}
+                  style={{ backgroundColor: plan.active ? "#3b8590" : "rgba(133,134,149,0.1)" }}>
+                  {plan.active && <Tick fill="white" />}
                 </div>
               </div>
               <div className="modal__plans--price">
@@ -320,11 +323,11 @@ const PayWallModal = (props) => {
               border: `1px solid ${pay.active ? "#3b8590" : '#36596a55'}`,
               backgroundColor: pay.active ? '#3b85911a' : 'white'
             }}
-                 onClick={() => changePayment(i)}
+              onClick={() => changePayment(i)}
             >
               <div className="modal__plans--tick"
-                   style={{backgroundColor: pay.active ? "#3b8590" : "rgba(133,134,149,0.1)"}}>
-                {pay.active && <Tick fill="white"/>}
+                style={{ backgroundColor: pay.active ? "#3b8590" : "rgba(133,134,149,0.1)" }}>
+                {pay.active && <Tick fill="white" />}
               </div>
               {pay.src}
             </div>
@@ -332,56 +335,56 @@ const PayWallModal = (props) => {
         </div>
         <form className="pay__modal--form" onSubmit={handleSubmit}>
           {activePayment === 'credit card'
-          && <div className="pay__modal--creditCard">
-            <div style={{display: 'flex'}}><label className="pay__form--firstName">
-              <span>First Name</span>
-              <input type="text" value={firstName} onChange={e => setFirstName(e.target.value)} required/>
-              <img src={penDot} alt="pen"/>
-            </label>
-              <label className="pay__form--lastName">
-                <span>Last Name</span>
-                <input type="text" value={lastName} onChange={e => setLastName(e.target.value)} required/>
-                <img src={penDot} alt="pen"/>
-              </label></div>
-            <label className="pay__form--creditCard">
-              <span>Card Number</span>
-              <CardNumberElement id="creditCard__number"/>
-              <img src={penDot} alt="pen"/>
-            </label>
-            <div className="creditCard__last">
-              <label>
-                <span>Expiry</span>
-                <CardExpiryElement id="creditCard__expire"/>
-                <img src={penDot} alt="pen"/>
+            && <div className="pay__modal--creditCard">
+              <div style={{ display: 'flex' }}><label className="pay__form--firstName">
+                <span>First Name</span>
+                <input type="text" value={firstName} onChange={e => setFirstName(e.target.value)} required />
+                <img src={penDot} alt="pen" />
               </label>
-              <label>
-                <span>CVV</span>
-                <CardCvcElement id="creditCard__cvv"/>
-                <img src={penDot} alt="pen"/>
+                <label className="pay__form--lastName">
+                  <span>Last Name</span>
+                  <input type="text" value={lastName} onChange={e => setLastName(e.target.value)} required />
+                  <img src={penDot} alt="pen" />
+                </label></div>
+              <label className="pay__form--creditCard">
+                <span>Card Number</span>
+                <CardNumberElement id="creditCard__number" />
+                <img src={penDot} alt="pen" />
               </label>
-            </div>
-          </div>}
-        
+              <div className="creditCard__last">
+                <label>
+                  <span>Expiry</span>
+                  <CardExpiryElement id="creditCard__expire" />
+                  <img src={penDot} alt="pen" />
+                </label>
+                <label>
+                  <span>CVV</span>
+                  <CardCvcElement id="creditCard__cvv" />
+                  <img src={penDot} alt="pen" />
+                </label>
+              </div>
+            </div>}
+
           {activePayment === 'paypal'
-          && <PaypalExpressBtn
-            env={env}
-            client={client}
-            currency={currency}
-            total={total}
-            onError={onError}
-            onCancel={onCancel}
-            onSuccess={onSuccess}
-            style={{
-              size: 'large',
-              color: 'blue',
-              shape: 'rect',
-              label: 'checkout'
-            }}
-          />}
+            && <PaypalExpressBtn
+              env={env}
+              client={client}
+              currency={currency}
+              total={total}
+              onError={onError}
+              onCancel={onCancel}
+              onSuccess={onSuccess}
+              style={{
+                size: 'large',
+                color: 'blue',
+                shape: 'rect',
+                label: 'checkout'
+              }}
+            />}
           {(activePayment === "apple pay" && paymentRequest) &&
-          <PaymentRequestButtonElement options={{paymentRequest}}/>}
-        
-          <h5 style={{marginTop: '30px'}}>Terms and Conditions</h5>
+            <PaymentRequestButtonElement options={{ paymentRequest }} />}
+
+          <h5 style={{ marginTop: '30px' }}>Terms and Conditions</h5>
           <div className="pay__modal--Conditions">
             <p>Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit</p>
             <p>sed Do Eiusmod Tempor Incididunt Ut Labore Et Dolore</p>
