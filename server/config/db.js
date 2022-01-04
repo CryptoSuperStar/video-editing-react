93a5c322bbcfff2d0ae6c36bc1ae36247093ce13
CryptoSuperStar <benjaminxu0828>
2022-01-04 16:17:56 +0000

    fixing mongoose package upgrade problems

--- server/config/db.js
+++ server/config/db.js
@@ -4,8 +4,6 @@ const connectDB = async () => {
 
   const connection = await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
     useUnifiedTopology: true
   });
 
