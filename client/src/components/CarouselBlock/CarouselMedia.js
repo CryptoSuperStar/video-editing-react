fd4670f24133274ae5906be1c3b4de451637f25e
Dev3 <dev3provid@gmail.com>
2022-02-10 18:36:33 +0530

    mvp-151 fixes

--- client/src/components/CarouselBlock/CarouselMedia.js
+++ client/src/components/CarouselBlock/CarouselMedia.js
@@ -5,7 +5,7 @@ import { connect } from "react-redux";
 import S3 from 'react-aws-s3';
 import { arrayMoveImmutable } from 'array-move';
 import { REACT_APP_AWS_KEY, REACT_APP_AWS_SECRET_KEY, REACT_APP_BUCKET } from "../../utils/misc";
-import { ReactComponent as Plus2 } from "../../assets/img/button.svg";
+import Plus2 from "../../assets/img/button.png";
 import './CarouselMedia.scss';
 import {
   addMediaToProject,
@@ -17,7 +17,10 @@ import {
 import { toast } from "react-toastify";
 import DraggableContentList from "../DraggableContent/DraggableContentList";
 import { mediaTypeVideo } from '../../utils/constant';
-import { FaDownload } from 'react-icons/fa'
+import Dropzone from 'react-dropzone';
+import MoonLoader from "react-spinners/MoonLoader";
+import { FaDownload } from 'react-icons/fa';
+import { ReactComponent as Cancel } from "../../assets/img/close-2.svg";
 
 const CarouselMedia = (props) => {
 
@@ -28,71 +31,114 @@ const CarouselMedia = (props) => {
   const [showDraggable, setShowDraggable] = useState(false);
   const [showArrow, setShowArrow] = useState(true);
   const [showModal, setShowModal] = useState(false);
+  const [uploadMedia, setUploadMedia] = useState(false);
+  const [notSupported, setNotSupported] = useState('');
+  const [loadingStatus, setLoadingStatus] = useState([]);
+  const [totalFilesFromDashboard, setTotalFilesFromDashboard] = useState(localStorage.getItem("TotalFiles"))
+  const [loadingDivsHtml, setLoadingDivsHtml] = useState([])
+  const [disableButtons, setDisableButtons] = useState(localStorage.getItem("disableButtons"))
+
+  let LoadingStatus = [];
 
   useEffect(() => {
-
     setContents(props.content);
+  }, [props.content, loadingStatus])
 
-
-  }, [props.content])
-
-  const handleChange = (e) => {
-    const { target } = e;
-    props.setComments([]);
-    props.setErrorMessage(null);
-    localStorage.removeItem('comments');
-    const file = e.target.files[0]; // accessing file
-    const fileSize = file.size / 1048576;
-    const fileName = (file.name).split('.');
-    let supportedTypes = ["wav","mp3","aac","ogg","oga","wma","flac","png","gif","avif","apng","jpg", "jpeg","svg","webp","bmp","ico","tiff","mp4","mov"]
-    if (fileSize > 2048) {
-      toast.error('The File size cannot exceed 2GB')
-      target.files = null;
-      target.value = null;
+  useEffect(() => {
+    let _loadingDivsHtml = []
+    for (let i = 0; i < parseInt(totalFilesFromDashboard); i++) {
+      _loadingDivsHtml.push(
+        <div key={i} className="mediaFiles__slider--inner" style={{ backgroundColor: 'black' }}>
+          <div style={{ display: 'flex', justifyContent: 'center', width: '100vw' }}>
+            <MoonLoader className="spinner" color=" #ffffff" loading={props.loadingVideo} size={50} />
+          </div>
+        </div>
+      )
     }
-    if(supportedTypes.includes(fileName[fileName.length - 1].toLowerCase()) === false ){
-      target.value = null;
-      target.files = null;
-      setShowModal(true);
+    setLoadingDivsHtml(_loadingDivsHtml)
+  }, [totalFilesFromDashboard])
+
+
+
+  const handleChange = (files) => {
+    localStorage.removeItem('TotalFiles');
+    let supportedFiles = [];
+    let unsupportedFiles = [];
+    let filesToUpload = [];
+    const config = {
+      bucketName: REACT_APP_BUCKET,
+      dirName: `${props.user._id}/${props.project.bucket}`,
+      region: 'us-east-2',
+      accessKeyId: REACT_APP_AWS_KEY,
+      secretAccessKey: REACT_APP_AWS_SECRET_KEY
     }
-    else {
-      let newFileName = e.target.files[0].name;
-      newFileName = newFileName.replace(/ /g, '_');
-      newFileName = newFileName.replace(/\(|\)/g, '');
-      const config = {
-        bucketName: REACT_APP_BUCKET,
-        dirName: `${props.user._id}/${props.project.bucket}`,
-        region: 'us-east-2',
-        accessKeyId: REACT_APP_AWS_KEY,
-        secretAccessKey: REACT_APP_AWS_SECRET_KEY
-      }
-      const ReactS3Client = new S3(config);
-      props.setLoadingVideo(true);
-      ReactS3Client.uploadFile(file, newFileName).then(data => {
+    const ReactS3Client = new S3(config);
+    const getTimeLine = (res) => {
+      let currentMedia = res.currentMedia;
+      localStorage.currentMedia = res.currentMedia._id;
+      props.setIsShowComment(false);
+      props.setShowShareModal(false);
+      props.setComments([]);
+      props.setErrorMessage(null);
+    },
+      addDataToProject = (data) => {
         if (data.status === 204) {
-          props.dispatch(addMediaToProject(data.location, localStorage.currentProjectId, props.project.bucket, props.setLoadingVideo))
-            .then((res) => {
-              let currentMedia = res.currentMedia;
-              localStorage.currentMedia = res.currentMedia._id;
-              props.setIsShowComment(false);
-              props.setShowShareModal(false);
-              props.setComments([]);
-              props.setErrorMessage(null);
-              if (res.mediaType === mediaTypeVideo) {
-                props.setLoadingSlider(true);
-                props.dispatch(takeScreenshots(
-                  res.project._id,
-                  props.project.bucket,
-                  currentMedia.mediaSrc,
-                  currentMedia.mediaName
-                  , props.setLoadingSlider))
-              }
-            })
-        } else {
+          return props.dispatch(addMediaToProject(data.location, localStorage.currentProjectId, props.project.bucket, props.setLoadingVideo))
+            .then(getTimeLine)
+        }
+        else {
           toast.error('The File was no uploaded to AWS')
         }
-      })
+      }, finalAction = async ({ file, newFileName }, ...restFiles) => {
+        LoadingStatus.pop();
+        ReactS3Client.uploadFile(file, newFileName).then(data => addDataToProject(data).then(
+          () => {
+            LoadingStatus && setLoadingStatus(LoadingStatus);
+            restFiles?.length && finalAction(...restFiles)
+          }
+        )
+        );
+        return restFiles;
+      };
+    let supportedTypes = ["wav", "mp3", "aac", "ogg", "oga", "wma", "flac", "png", "gif", "avif", "apng", "jpg", "jpeg", "svg", "webp", "bmp", "ico", "tiff", "mp4", "mov"]
+    for (let file of files) {
+      const fileName = (file.name).split('.').pop();
+      {
+        !supportedTypes.includes(fileName.toLowerCase()) ? unsupportedFiles.push(file) : supportedFiles.push(file) ? LoadingStatus.push(1) : console.log('undefined')
+      }
+      LoadingStatus && setLoadingStatus(LoadingStatus);
     }
+    if (unsupportedFiles?.length) {
+      unsupportedFiles.forEach((file) => {
+        setNotSupported(file.name);
+      })
+      setUploadMedia(false);
+      setShowModal(true);
+    }
+    supportedFiles.forEach((e) => {
+      props.setComments([]);
+      props.setErrorMessage(null);
+      localStorage.removeItem('comments');
+      const file = e;
+      const fileSize = file.size / 1048576;
+      if (fileSize > 2048) {
+        setUploadMedia(false);
+        toast.error('The File size should be less than 2GB')
+      }
+      else {
+        setUploadMedia(false);
+        props.setLoadingVideo(true);
+        let newFileName = file.name;
+        newFileName = newFileName.replace(/ /g, '_');
+        newFileName = newFileName.replace(/\(|\)/g, '');
+        filesToUpload.push({ file, newFileName });
+      }
+    });
+    finalAction(...filesToUpload).then(() => {
+      if (unsupportedFiles?.length) {
+        setShowModal(true);
+      }
+    });
   }
 
   const onSortEnd = ({ oldIndex, newIndex }) => {
@@ -108,9 +154,6 @@ const CarouselMedia = (props) => {
     newContent[index] = newCurrentMedia;
     if (localStorage.comments && newCurrentMedia.screens.length) {
       let newComments = JSON.parse(localStorage.comments);
-      // newCurrentMedia = newCurrentMedia.screens.map((item, i) => {
-      //   return newComments[i].text.length > 0 ? { ...item, comment: newComments[i] } : item
-      // })
       newContent[index].comments = newComments;
       props.setComments([]);
     }
@@ -154,7 +197,8 @@ const CarouselMedia = (props) => {
         {contents.map(media => (
           <>
             <div className="mediaFiles__slider--inner" key={media._id}
-              style={{border: localStorage.currentMedia === media._id ? `8px solid hsl(229deg 82% 11%)` : props.editedProject._id === media._id && `8px solid #4ea0d6`,
+              style={{
+                border: localStorage.currentMedia === media._id ? `8px solid hsl(229deg 82% 11%)` : props.editedProject._id === media._id && `8px solid #4ea0d6`,
                 background: (media.isImage || (media.screens && media.screens.length > 0))
                   ? `url(${media.isImage ? media.mediaSrc : media.screens[1].screenSrc})` : 'black'
               }}
@@ -177,8 +221,11 @@ const CarouselMedia = (props) => {
               }
               }
             >
+              {(props.loadingVideo && !media.isImage) && (<div style={{ display: 'flex', justifyContent: 'center', width: '100vw' }}>
+                {(props.loadingVideo && !media.isImage) && <MoonLoader className="spinner" color=" #ffffff" loading={props.loadingVideo} size={50} />}
+              </div>)}
               {props.editedProject._id === media._id && <span className='vertical_line'></span>}
-              <p>{media.mediaName}</p>
+              {!props.loadingVideo && (<p>{media.mediaName}</p>)}
               {props.isEditor && <a href={`${media.mediaSrc}`} target="_blank" rel="noreferrer" download={`${media.mediaSrc}`} title='Download'> <span className="download__video--btn">
                 <FaDownload size={"30px"} />
               </span></a>}
@@ -187,30 +234,34 @@ const CarouselMedia = (props) => {
             </div>
           </>
         ))}
-        {(props.project.projectStatus === "Draft" || (props.isEditor && props.project.projectStatus !== "Done")) && <div className="mediaFiles__slider--inner" ref={sliderItemWidth}
-          onClick={() => {
-            if ((localStorage.updateComment && localStorage.updateComment === 'true')
-              || (localStorage.editedVideoTime && localStorage.editedVideoTime === 'true')) {
-              updateComments(localStorage.currentMedia);
+        {loadingStatus && loadingStatus?.length > 0 && loadingStatus?.map((e) => (<div className="mediaFiles__slider--inner" style={{ backgroundColor: 'black' }}>
+          <div style={{ display: 'flex', justifyContent: 'center', width: '100vw' }}>
+            <MoonLoader className="spinner" color=" #ffffff" loading={props.loadingVideo} size={50} />
+          </div>
+        </div>))}
+        {loadingDivsHtml}
+        {(props.project.projectStatus === "Draft" || (props.isEditor && props.project.projectStatus !== "Done")) &&
+          <div className="mediaFiles__slider--inner" ref={sliderItemWidth}
+            style={disableButtons && { pointerEvents: 'none' }}
+            onClick={() => {
+              if ((localStorage.updateComment && localStorage.updateComment === 'true')
+                || (localStorage.editedVideoTime && localStorage.editedVideoTime === 'true')) {
+                updateComments(localStorage.currentMedia);
+              }
+              setUploadMedia(true);
             }
-          }
-          }
-        >
-          <input type="file" ref={fileInput} onChange={(e) => {
-            handleChange(e)
-          }} id="mediaFiles__button"
-            accept="audio/*,video/*,image/*"
-          />
-          <label htmlFor="mediaFiles__button">
-            <Plus2 />
-          </label>
-          <span className="tip_icon" style={{ marginTop: "6px", marginRight: "5px" }}>
-            i
-            <span className="info" style={{ marginTop: "10px", marginLeft: "-100px" }}>
-              Add more media files to your project.
+            }
+          >
+            <label htmlFor="mediaFiles__button">
+              <img src={Plus2} className='corousel_img'></img>
+            </label>
+            <span className="tip_icon" style={{ marginTop: "6px", marginRight: "5px" }}>
+              i
+              <span className="info" style={{ marginTop: "10px", marginLeft: "-100px" }}>
+                Add more media files to your project.
+              </span>
             </span>
-          </span>
-        </div>}
+          </div>}
       </Carousel>
         :
         <DraggableContentList
@@ -229,10 +280,37 @@ const CarouselMedia = (props) => {
                   <Cancel fill="black" className="connectSocial__cross--cancel" />
                   <ArrowLeft className="connectSocial__cross--arrowLeft" />
               </div> */}
-            <h3>File not supported</h3>
+            <h3>{notSupported} not supported</h3>
             <button className="pay__modal--submit" type="submit" onClick={() => { setShowModal(false) }}>Ok</button>
           </div>
         </div>)
+      }
+      {uploadMedia && <div className=" modal__wrapper"><div className="style__modal uploadFiles_model">
+        <div className="connectSocial__cross" onClick={() => { setUploadMedia(false) }}>
+          <Cancel fill="black" />
+        </div>
+        <Dropzone onDrop={(e) => {
+          handleChange(e);
+        }}>
+          {({ getRootProps, getInputProps }) => ((
+            <div {...getRootProps()}>
+              <h2 className='dropzone_header'>Upload Media</h2>
+              <p className='dropzone_paragraph'>Drag 'n' drop some files here, or click to select files</p>
+              <div className='dropzone_css' style={{ display: 'flex', justifyContent: 'center', width: "57vw" }}>
+                <div style={{ width: "10vw" }}>
+                  <label htmlFor="mediaFiles__button" className='plus_alignment dropzone_css'>
+                    <img src={Plus2} className='dropzone_img'></img>
+                  </label>
+                </div>
+                <input {...getInputProps()}
+                  multiple='multiple'
+                  accept="audio/*,video/*,image/*"
+                />
+              </div>
+            </div>))}
+        </Dropzone>
+      </div>
+      </div>
       }
     </div >
   );
--- client/src/components/EmptyProject/EmptyProject.js
+++ client/src/components/EmptyProject/EmptyProject.js
@@ -8,12 +8,19 @@ import { createTempProject, takeScreenshots } from "../../store/actions/project.
 import { toast } from "react-toastify";
 import DemoLayerEmpty from "../DemoLayer/DemoLayerEmpty";
 import { mediaTypeVideo } from '../../utils/constant';
+import Dropzone from 'react-dropzone';
+import Plus2 from "../../assets/img/button.png";
+import MoonLoader from "react-spinners/MoonLoader";
+import { ReactComponent as Cancel } from "../../assets/img/close-2.svg";
 
 const EmptyProject = (props) => {
 
   const fileInput = useRef();
   const [showDemo, setShowDemo] = useState(false);
   const [showModal, setShowModal] = useState(false);
+  const [uploadMedia, setUploadMedia] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [notSupported, setNotSupported] = useState('');
 
   useEffect(() => {
     if (localStorage.showDemoLayer === 'true') {
@@ -21,72 +28,100 @@ const EmptyProject = (props) => {
     }
   }, [])
 
-  const handleChange = (e) => {
-    const { target } = e;
+  const handleChange = (files) => {
     props.setComments([])
-    const file = e.target.files[0]; // accessing file
-    const fileSize = file.size / 1048576;
-    const fileName = (file.name).split('.');
-    let supportedTypes = ["wav","mp3","aac","ogg","oga","wma","flac","png","gif","avif","apng","jpg", "jpeg","svg","webp","bmp","ico","tiff","mp4","mov"]
-    if (fileSize > 2048) {
-      toast.error('The File size should be less than 2GB')
-      target.files = null;
-      target.value = null;
-    }
-    if(supportedTypes.includes(fileName[fileName.length - 1].toLowerCase()) === false ){
-      setShowModal(true);
-      target.files = null;
-      target.value = null;
-    }
-    else {
-      let newFileName = e.target.files[0].name;
-      newFileName = newFileName.replace(/ /g, '_')
-      newFileName = newFileName.replace(/\(|\)/g, '');
-      const bucket = uuidv4();
-      const config = {
-        bucketName: REACT_APP_BUCKET,
-        dirName: `${props.user._id}/${bucket}`,
-        region: 'us-east-2',
-        accessKeyId: REACT_APP_AWS_KEY,
-        secretAccessKey: REACT_APP_AWS_SECRET_KEY
+    let unsupportedFiles = [];
+    let supportedFiles = [];
+    let supportedTypes = ["wav", "mp3", "aac", "ogg", "oga", "wma", "flac", "png", "gif", "avif", "apng", "jpg", "jpeg", "svg", "webp", "bmp", "ico", "tiff", "mp4", "mov"]
+    for (let file of files) {
+      const fileName = (file.name).split('.').pop();
+      {
+        !supportedTypes.includes(fileName.toLowerCase()) ? unsupportedFiles.push(file) : supportedFiles.push(file)
       }
-      const ReactS3Client = new S3(config);
-      props.setLoadingVideo(true);
-      ReactS3Client.uploadFile(file, newFileName).then(data => {
-        if (data.status === 204) {
-          props.dispatch(createTempProject(data.location, bucket, props.setLoadingVideo)).then((res) => {
-            if (res.mediaType === mediaTypeVideo) {
-              props.setLoadingSlider(true);
-              props.dispatch(takeScreenshots(
-                res.project._id,
-                res.project.bucket,
-                res.project.content[0].mediaSrc,
-                res.project.content[0].mediaName
-                , props.setLoadingSlider))
-            }
-          })
-        } else {
-          toast.error('The File was no uploaded to AWS')
+    }
+    for (let e in files) {
+      let file = files[e];
+      const fileSize = file.size / 1048576;
+      const fileName = (file.name).split('.');
+      if (fileSize > 2048) {
+        toast.error('The File size should be less than 2GB')
+      }
+      if (supportedTypes.includes(fileName[fileName.length - 1].toLowerCase()) === false) {
+        setNotSupported(file.name);
+        setUploadMedia(false);
+        setShowModal(true);
+      }
+      else {
+        setUploadMedia(false);
+        let newFileName = file.name;
+        newFileName = newFileName.replace(/ /g, '_')
+        newFileName = newFileName.replace(/\(|\)/g, '');
+        const bucket = uuidv4();
+        const config = {
+          bucketName: REACT_APP_BUCKET,
+          dirName: `${props.user._id}/${bucket}`,
+          region: 'us-east-2',
+          accessKeyId: REACT_APP_AWS_KEY,
+          secretAccessKey: REACT_APP_AWS_SECRET_KEY
         }
-      })
+        const ReactS3Client = new S3(config);
+        setLoading(true);
+        props.setLoadingVideo(true);
+        ReactS3Client.uploadFile(file, newFileName).then(data => {
+          if (data.status === 204) {
+            if (files[e] == files[0]) {
+              props.dispatch(createTempProject(data.location, bucket, props.setLoadingVideo)).then((res) => {
+                localStorage.setItem("currentProjectId", res.project._id);
+                let format = ['mp4', 'mov'];
+                let lastFormat = data.key.split('.').pop();
+                { !format.includes(lastFormat) && props.setLoadingVideo(false) }
+                if (res.mediaType === mediaTypeVideo) {
+                  setLoading(false);
+                  props.setLoadingSlider(true);
+                  props.dispatch(takeScreenshots(
+                    res.project._id,
+                    res.project.bucket,
+                    res.project.content[0].mediaSrc,
+                    res.project.content[0].mediaName
+                    , props.setLoadingSlider,
+                    props.setLoadingVideo))
+                }
+              })
+            }
+            else if (e > 0) {
+              localStorage.setItem("TotalFiles", supportedFiles.length - 1)
+              localStorage.setItem("disableButtons", true)
+              props.setLoadingVideo(true);
+            }
+          }
+          else {
+            toast.error('The File was no uploaded to AWS')
+          }
+        })
+      }
     }
   }
 
   return (
     <>
       {showDemo && <DemoLayerEmpty setShowDemo={setShowDemo} />}
-      <img src={uploadImage} alt="upload_image" />
-      <h3>Upload videos to start!</h3>
-      <p>Select media in the order you want it to be used.</p>
-      <div className="upload__media--field" >
-        <input type="file" ref={fileInput} id="upload__media--button"
-          onChange={handleChange}
-          accept="audio/*,video/*,image/*"
-        />
-        <label htmlFor="upload__media--button">
-          Upload Media
-        </label>
-      </div>
+      {loading ? <div className="spinner__wrapper" style={{ height: '100vh' }}>
+
+        <MoonLoader className="spinner" color="#000" loading={loading} size={50} />
+        <div style={{ padding: "20px" }}>Uploading....  Please wait </div>
+
+      </div> :
+        <div class='upload_media_allignment'>
+          <img src={uploadImage} alt="upload_image" />
+          <h3>Upload videos to start!</h3>
+          <p className='Upload_media_paragraph'>Select media in the order you want it to be used.</p>
+          <div className="upload__media--field" >
+            <label htmlFor="upload__media--button" onClick={() => setUploadMedia(true)}>
+              Upload Media
+            </label>
+          </div>
+        </div>
+      }
       {
         showModal &&
         (<div className=" modal__wrapper">
@@ -100,6 +135,35 @@ const EmptyProject = (props) => {
           </div>
         </div>)
       }
+      {uploadMedia && <div className=" modal__wrapper">
+        <div className="style__modal uploadFiles_model">
+          <div className="connectSocial__cross" onClick={() => { setUploadMedia(false) }}>
+            <Cancel fill="black" />
+          </div>
+          <Dropzone onDrop={(e) => {
+            handleChange(e);
+          }}>
+            {({ getRootProps, getInputProps }) => ((
+              <div {...getRootProps()}>
+                <h2 className='dropzone_header'>Upload Media</h2>
+                <p className='dropzone_paragraph'>Drag 'n' drop some files here, or click to select files</p>
+                <div className='dropzone_css' style={{ display: 'flex', justifyContent: 'center', width: "56vw" }}>
+                  <div style={{ width: "10vw" }}>
+                    <label htmlFor="mediaFiles__button" className='plus_alignment dropzone_css'>
+                      <img src={Plus2} className='dropzone_img'></img>
+                    </label>
+                  </div>
+                  <input {...getInputProps()}
+                    multiple='multiple'
+                    accept="audio/*,video/*,image/*"
+                  />
+                </div>
+              </div>))}
+          </Dropzone>
+        </div>
+      </div>
+      }
+
     </>
   );
 };
