59945366e7fb53ad97b658d903f2d38713a06d3d
Elnura Amatova <elnura@rav.ai>
2022-02-24 01:42:10 +0000

    Revert "Integration (pull request #403)"

--- server/routes/share.route.js
+++ server/routes/share.route.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const {uploadYouTube, authYouTube,downloadFile} = require('../controllers/share.controller');
+const {uploadYouTube, authYouTube} = require('../controllers/share.controller');
 
 router.get('/authYouTube', authYouTube);
 router.post('/uploadToYouTube', uploadYouTube);
-router.get('/downloadFile/:project_id/:bucket/:mediaName', downloadFile);
+
 module.exports = router;
